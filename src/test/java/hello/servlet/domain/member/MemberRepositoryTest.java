3ca4a7d3baef27c08dde4e17f03bd4bd5be939ea
heechul <khc41@lgensol.com>
2023-06-19 21:50:20 +0900

    member repository 생성

--- src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
+++ src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -1,12 +1,11 @@
 package hello.servlet.domain.member;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class MemberRepositoryTest {
 
@@ -19,30 +18,29 @@ class MemberRepositoryTest {
 
     @Test
     void save() {
-        //given
+        // Given
         Member member = new Member("hello", 20);
 
-        //when
+        // When
         Member savedMember = memberRepository.save(member);
 
-        //then
+        // Then
         Member findMember = memberRepository.findById(savedMember.getId());
         assertThat(findMember).isEqualTo(savedMember);
     }
 
     @Test
     void findAll() {
-        //given
+        // Given
         Member member1 = new Member("member1", 20);
         Member member2 = new Member("member2", 30);
 
         memberRepository.save(member1);
         memberRepository.save(member2);
-
-        //when
+        // When
         List<Member> result = memberRepository.findAll();
 
-        //then
+        // Then
         assertThat(result.size()).isEqualTo(2);
         assertThat(result).contains(member1, member2);
     }
