20e341dd4db1d85a403e4aeb9c580c7971a86821
heechul <khc4190@naver.com>
2023-09-23 19:06:37 +0900

    final 추가

--- src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberListControllerV3.java
+++ src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberListControllerV3.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Map;
 
 public class MemberListControllerV3 implements ControllerV3 {
-    private MemberRepository memberRepository = MemberRepository.getInstance();
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
     public ModelView process(Map<String, String> paramMap) {
